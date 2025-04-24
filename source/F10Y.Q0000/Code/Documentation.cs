53f7bea7853ac96db58766f5b2721a5de0f47706
davidcoats <david@getsafetycone.com>
2025-04-24 10:36:17 -0700

    Initial commit.

---
+++ source/F10Y.Q0000/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace F10Y.Q0000
+{
+	/// <summary>
+	/// General C#/.NET framework examinations.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/F10Y.Q0000/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace F10Y.Q0000
+{
+    public static class Instances
+    {
+		
+    }
+}
